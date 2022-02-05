e569e49c5d6e058b097148fd8df36e1e674a904f
suy2on <tnwjdrms@naver.com>
2022-02-05 13:51:04 +0900

    # feat : 알고리즘 테스트

---
+++ src/LV2/Main.java
@@ -0,0 +1,9 @@
+package LV2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        One one = new One();
+        System.out.println(one.solution(new int[]{2, 1, 3, 2}, 2));
+    }
+}
