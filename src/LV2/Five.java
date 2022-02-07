bbc29e4834b29c343f88757ea3a17226c06cff46
suy2on <tnwjdrms@naver.com>
2022-02-07 16:03:38 +0900

    # feat : 소수찾기 dfs

---
+++ src/LV2/Five.java
@@ -0,0 +1,43 @@
+package LV2;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Five {
+    Set<Integer> primeSet = new HashSet<>();
+
+    public Boolean isPrime(int num){
+
+        if(num <= 1) return Boolean.FALSE;
+        int mid = num / 2;
+        for(int i =  2; i<mid+1; i++){
+            if(num % i == 0) return Boolean.FALSE;
+        }
+
+        return Boolean.TRUE;
+    }
+
+    public void dfs(List<String> num_list, String part){
+        //System.out.println("dfs : " + num_list + " part = " + part);
+        if(num_list.size() == 0){
+            return ;
+        }
+        for(String n : num_list){
+            if(isPrime(Integer.parseInt(part + n))){
+                primeSet.add(Integer.parseInt(part + n));
+               // System.out.println("prime");
+            }
+            List<String> copy_list = new ArrayList<>(num_list.subList(0, num_list.size()));
+            copy_list.remove(n);
+            dfs(copy_list, part + n);
+        }
+    }
+
+    public int solution(String numbers) {
+        List<String> nums = Arrays.stream(numbers.split("")).collect(Collectors.toList());
+        dfs(nums, "");
+
+
+        return primeSet.size();
+    }
+}
--- src/LV2/Main.java
+++ src/LV2/Main.java
@@ -3,7 +3,7 @@ package LV2;
 public class Main {
 
     public static void main(String[] args) {
-        One one = new One();
-        System.out.println(one.solution(new int[]{2, 1, 3, 2}, 2));
+        Five five = new Five();
+        System.out.println(five.solution("011"));
     }
 }
