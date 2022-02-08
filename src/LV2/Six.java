ac1926cd6eb054ea41c41c7c6108908915e85f37
suy2on <tnwjdrms@naver.com>
2022-02-08 14:51:25 +0900

    # feat : 정렬최적화

--- src/LV2/Six.java
+++ src/LV2/Six.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Six {
     public int solution(int[] citations) {
         int answer = 0;
-        citations = Arrays.stream(citations).sorted().toArray(); // 정렬
+        Arrays.sort(citations); // 정렬
 
         int i = 0;
         for(int citation : citations){
