5f1cf5f6429e88e279efb94327eeae72c31ddf51
suy2on <tnwjdrms@naver.com>
2022-02-13 16:30:15 +0900

    # feat : 그리디 문제 3 완료

---
+++ src/LV2/Eleven.java
@@ -0,0 +1,36 @@
+package LV2;
+
+import java.util.Stack;
+
+public class Eleven {
+    public String solution(String number, int k) {
+        String answer = "";
+        Stack<Character> nums = new Stack();
+        int remove = 0;
+        int i;
+
+        if(number.length() == 1){
+            return number;
+        }
+
+        for(i=0; i<number.length(); i++){
+            while(remove < k && !nums.isEmpty() && Character.compare(nums.peek(), number.charAt(i)) < 0){
+                nums.pop();
+                remove++;
+            }
+
+            if(remove >= k) break;
+
+            nums.push(number.charAt(i));
+        }
+
+        for(Character c : nums){
+            answer += Character.toString(c);
+        }
+        
+
+        if(remove < k) return answer.substring(0,answer.length()-1);
+
+        return answer + number.substring(i);
+    }
+}
