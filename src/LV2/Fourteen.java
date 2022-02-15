1d86a6d68488eabf0ae22e519bb5a69c7f9794e6
suy2on <tnwjdrms@naver.com>
2022-02-15 17:11:08 +0900

    # feat : 주석추가

--- src/LV2/Fourteen.java
+++ src/LV2/Fourteen.java
@@ -5,8 +5,8 @@ public class Fourteen {
     int target;
 
     public void dfs(int total, int i, int[] numbers){
-        if(i >= numbers.length){
-            if(total == target) answer++;
+        if(i >= numbers.length){ // 끝에 다다르면 일단 멈춤
+            if(total == target) answer++; // answer이면 count 
             return;
         }
         dfs(total+numbers[i], i+1, numbers);
